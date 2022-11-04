39a6775ec248c3bb0dd960bc4745be675ebe6579
Zsolt Márk Farkas <zsolt.farkas2@stud.uni-corvinus.hu>
2022-11-04 23:38:54 +0100

    utolso valtoztatasok

---
+++ hatodikfeladat/hatodikfeladat/Entities/Present.cs
@@ -0,0 +1,18 @@
+﻿using hatodikfeladat.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hatodikfeladat.Entities
+{
+    public class Present : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            
+        }
+    }
+}
