48e84a2fa09c60d29ce4b9eb60f82db7ef695fea
Zsolt Márk Farkas <zsolt.farkas2@stud.uni-corvinus.hu>
2022-11-04 22:03:05 +0100

    Gyár absztrakció kezelése

---
+++ hatodikfeladat/hatodikfeladat/Abstractions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hatodikfeladat.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
