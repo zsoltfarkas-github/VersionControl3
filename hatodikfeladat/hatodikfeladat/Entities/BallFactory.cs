c78818f3fdb520dbf0b74f10a6ecb2c1355b2067
Zsolt Márk Farkas <zsolt.farkas2@stud.uni-corvinus.hu>
2022-11-04 23:35:45 +0100

    Labda színezése

--- hatodikfeladat/hatodikfeladat/Entities/BallFactory.cs
+++ hatodikfeladat/hatodikfeladat/Entities/BallFactory.cs
@@ -1,6 +1,7 @@
 ﻿using hatodikfeladat.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,11 @@ namespace hatodikfeladat.Entities
 {
     public class BallFactory : IToyFactory
     {
+        public Color BallColor { get; set; }
+
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Ball(BallColor);
         }
     }
 }
--- hatodikfeladat/hatodikfeladat/Form1.cs
+++ hatodikfeladat/hatodikfeladat/Form1.cs
@@ -72,7 +72,10 @@ namespace hatodikfeladat
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Factory = new BallFactory();
+            Factory = new BallFactory
+            {
+                BallColor = button3.BackColor
+            };
         }
 
         private void DisplayNext()
