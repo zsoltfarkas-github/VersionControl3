c696d7c731d4e191256f1fc9f97a923b66c35f05
Zsolt Márk Farkas <zsolt.farkas2@stud.uni-corvinus.hu>
2022-11-04 21:10:50 +0100

    labdagyar letrehozasa

---
+++ hatodikfeladat/hatodikfeladat/Entities/BallFactroy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hatodikfeladat.Entities
+{
+    class BallFactroy
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
