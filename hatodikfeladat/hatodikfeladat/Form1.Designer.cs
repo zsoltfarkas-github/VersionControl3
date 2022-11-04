ecda013ff64e9c015a9d215e554738e4e43f270c
Zsolt Márk Farkas <zsolt.farkas2@stud.uni-corvinus.hu>
2022-11-04 23:22:34 +0100

    Színválasztó gomb

--- hatodikfeladat/hatodikfeladat/Form1.Designer.cs
+++ hatodikfeladat/hatodikfeladat/Form1.Designer.cs
@@ -36,13 +36,14 @@ namespace hatodikfeladat
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
-            this.mainPanel.Location = new System.Drawing.Point(12, 96);
+            this.mainPanel.Location = new System.Drawing.Point(12, 126);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(776, 342);
+            this.mainPanel.Size = new System.Drawing.Size(776, 312);
             this.mainPanel.TabIndex = 0;
             // 
             // createTimer
@@ -86,11 +87,22 @@ namespace hatodikfeladat
             this.label1.TabIndex = 2;
             this.label1.Text = "Coming Next: ";
             // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(132, 63);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -110,6 +122,7 @@ namespace hatodikfeladat
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
