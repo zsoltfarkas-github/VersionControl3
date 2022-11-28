2d23070cb6afda7dde51d4fb389bda269f89c4f6
Zsolt Márk Farkas <zsolt.farkas2@stud.uni-corvinus.hu>
2022-11-29 00:14:06 +0100

    felulet epitese

--- kilencedikfeladat/Form1.Designer.cs
+++ kilencedikfeladat/Form1.Designer.cs
@@ -44,18 +44,18 @@ namespace kilencedikfeladat
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Záróév";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(189, 13);
+            this.label2.Location = new System.Drawing.Point(180, 13);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(71, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Népesség fájl";
             // 
             // button1
             // 
