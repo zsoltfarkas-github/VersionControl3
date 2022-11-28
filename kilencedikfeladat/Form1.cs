65f9ef2b6e10a8d85a7683df7c74b632224bf0c4
Zsolt Márk Farkas <zsolt.farkas2@stud.uni-corvinus.hu>
2022-11-28 23:25:40 +0100

    Véletlenszám generálás

--- kilencedikfeladat/Form1.cs
+++ kilencedikfeladat/Form1.cs
@@ -19,13 +19,17 @@ namespace kilencedikfeladat
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
 
+        Random rng = new Random(1234);
+
         public Form1()
         {
             InitializeComponent();
 
-            Population = GetPopulation(@"C:\Temp\nép.csv");
-            BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
-            DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+            Population = GetPopulation(@"C:\Windows\Temp\nép.csv");
+            BirthProbabilities = GetBirthProbabilities(@"C:\Windows\Temp\születés.csv");
+            DeathProbabilities = GetDeathProbabilities(@"C:\Windows\Temp\halál.csv");
+
+            dataGridView1.DataSource = BirthProbabilities;
         }
 
         public List<Person> GetPopulation(string csvpath)
