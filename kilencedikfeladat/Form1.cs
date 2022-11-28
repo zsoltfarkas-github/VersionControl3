2afd4ccc05f5b21c85206b489f305fc4768e6abd
Zsolt Márk Farkas <zsolt.farkas2@stud.uni-corvinus.hu>
2022-11-28 23:26:45 +0100

    Szimuláció

--- kilencedikfeladat/Form1.cs
+++ kilencedikfeladat/Form1.cs
@@ -29,7 +29,24 @@ namespace kilencedikfeladat
             BirthProbabilities = GetBirthProbabilities(@"C:\Windows\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Windows\Temp\halál.csv");
 
-            dataGridView1.DataSource = BirthProbabilities;
+            // Végigmegyünk a vizsgált éveken
+            for (int year = 2005; year <= 2024; year++)
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
 
         public List<Person> GetPopulation(string csvpath)
